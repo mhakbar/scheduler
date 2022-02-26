ab80bd02fe0b62089395ec2e2c49be33c89ca63b
mhakbar <habibakbar@live.com>
2022-02-25 17:33:37 -0800

    stories for InterviewerListItem added to stories/index.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,6 +7,7 @@ import DayList from "./DayList";
 export default function Application(props) {
 
   const [day, setDay] = useState('Monday');
+  console.log(day);
 
   const days = [
     {
--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,7 @@ import "index.scss";
 import DayListItem from "/Users/macbar/Documents/LHL/second_try/w6/scheduler/src/components/DayListItem.js";
 import Button from "components/Button";
 import DayList from "/Users/macbar/Documents/LHL/second_try/w6/scheduler/src/components/DayList.js";
+import InterviewerListItem from "/Users/macbar/Documents/LHL/second_try/w6/scheduler/src/components/InterviewerListItem";
 
 storiesOf("Button", module)
   .addParameters({
@@ -66,3 +67,39 @@ storiesOf("DayList", module)
   .add("Wednesday", () => (
       <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
   ));
+
+
+  //////InterviewListItem.js
+  const interviewer = {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png"
+  };
+  
+  storiesOf("InterviewerListItem", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Unselected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      />
+    ))
+    .add("Selected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected
+      />
+    ))
+    .add("Clickable", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={action("setInterviewer")}
+      />
+    ));
