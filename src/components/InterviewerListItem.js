6f1efe0b860a2a6adbedcb3f7b30a93a3abc1442
mhakbar <habibakbar@live.com>
2022-02-25 17:31:57 -0800

    InterviewerListItem JS and SCSS created

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+
+ const {id, name, avatar, selected, setInterviewer} = props;
+
+// const interviewer = {
+//   id: 1,
+//   name: "Sylvia Palmer",
+//   avatar: "https://i.imgur.com/LpaY82x.png"
+// };
+
+const interviewerClass = classNames('interviewers__item', {
+  
+  'interviewers__item--selected': selected
+});
+
+
+return (
+  <li onClick= {() => setInterviewer(id)} className={interviewerClass}>
+  <img
+    className="interviewers__item-image"
+    src={props.avatar}
+    alt={props.name}
+  />
+  {props.name }
+</li>
+);
+
+};
