879b2078d6d1d5a7c52ad40942a5c5409ed39a72
mhakbar <habibakbar@live.com>
2022-02-28 17:54:36 -0800

    Mock data added to Application.js and is being iterated over

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,8 +2,46 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
-
-
+import "components/Appointment";
+import Appointment from "components/Appointment";
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 export default function Application(props) {
 
   const [value, onChange] = useState('Monday');
@@ -42,6 +80,11 @@ export default function Application(props) {
         <img className="sidebar__lhl sidebar--centered"  src="images/lhl.png" alt="Lighthouse Labs"/>
         </section>
       <section className="schedule">
+        {appointments.map(appointment => (
+          <Appointment key={appointment.id} {...appointment} />
+
+        ))}
+        <Appointment key="last" time="5pm" />
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
