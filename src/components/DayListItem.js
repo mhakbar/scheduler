be499024907183cfbf57fe6bef4acb934a4684c6
mhakbar <habibakbar@live.com>
2022-03-09 14:34:40 -0800

    props decontructed properly in DayListItem.js

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,8 +7,8 @@ export default function DayListItem(props) {
   const {name, spots, selected, setDay} = props;
 
   const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": !props.spots
+    "day-list__item--selected": selected,
+    "day-list__item--full": !spots
   })
 
   const formatSpots = (spots) => {
@@ -22,8 +22,8 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
-      <h2 className="text--regular">{props.name}</h2> 
+    <li className={dayClass} onClick={() => setDay(name)} selected={selected}>
+      <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
